522155a9cb9c84274ad313c15bbedda364f3a242
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-02-19 00:04:33 +0800

    EP12Finished_brackeys_0219

--- easy/Assets/Script/BuildManager.cs
+++ easy/Assets/Script/BuildManager.cs
@@ -20,11 +20,19 @@ public class BuildManager : MonoBehaviour
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
     public void BuildTurretOn(Node node)
     {
+        if(PlayerStats.Money < turretToBuild.cost)
+        {
+            Debug.Log("not enough money");
+            return;
+        }
+        PlayerStats.Money -= turretToBuild.cost;
         GameObject turret = Instantiate(turretToBuild.prefab, node.transform.position + node.positionOffset, Quaternion.identity);
         node.turret = turret;
+        Debug.Log("turret build, money left: " + PlayerStats.Money);
     }
 
     public void SelectTurretToBuild(TurretBlueprint turret)
--- easy/Assets/Script/Node.cs
+++ easy/Assets/Script/Node.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
     public Color hoverColor;
+    public Color noMoneyColor;
     public Vector3 positionOffset;
 
     [Header("Optional")]
@@ -21,6 +23,9 @@ public class Node : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
         if (!BuildManager.instance.CanBuild)
             return;
 
@@ -37,7 +42,15 @@ public class Node : MonoBehaviour
     {
         if(!BuildManager.instance.CanBuild)
             return;
-        rend.material.color = hoverColor;
+
+        if (BuildManager.instance.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = noMoneyColor;
+        }
     }
 
     void OnMouseExit()
