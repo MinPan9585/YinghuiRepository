26fbfaaea52274ec854dc869821674b4cb1e478c
ZhangTsingtao <rickzhangqt@gmail.com>
2023-06-08 23:38:03 +0800

    Vertical Tower Switch path movement

---
+++ easy/Assets/Script/EventSystem/SwitchPathMovement_B_F.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SwitchPathMovement_B_F : MonoBehaviour
+{
+    [Header("ID")]
+    public int id;
+    [Header("Movement")]
+    public bool isRotate;
+    public Vector3 altTrans;
+
+    private Vector3 iniTrans;
+    private bool alt;
+
+    private void Start()
+    {
+        GameEvents.Instance.OnSwitchPath += Back_Forth;
+
+        alt = true;
+        if (isRotate)
+        {
+            iniTrans = transform.rotation.eulerAngles;
+        }
+        else
+        {
+            //iniTrans = transform.position;
+            iniTrans = transform.localPosition;
+        }
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.Instance.OnSwitchPath -= Back_Forth;
+    }
+
+    public void Back_Forth(int id)
+    {
+        StartCoroutine(B_F());
+    }
+    IEnumerator B_F()
+    {
+        SwitchPath(id);
+        yield return new WaitForSeconds(1f);
+        SwitchPath(id);
+    }
+
+    private void SwitchPath(int id)
+    {
+        Debug.Log("DOOOOOO");
+        if (id == this.id)
+        {
+            if (isRotate)
+            {
+                if (alt)
+                    //transform.rotation = Quaternion.Euler(altTrans);
+                    LeanTween.rotate(gameObject, altTrans, 0.2f).setEase(LeanTweenType.easeOutQuad);
+                else
+                    LeanTween.rotate(gameObject, iniTrans, 0.2f).setEase(LeanTweenType.easeOutQuad);
+            }
+            else
+            {
+                if (alt)
+                    //transform.position = altTrans;
+                    LeanTween.moveLocal(gameObject, altTrans, 0.2f).setEase(LeanTweenType.easeOutQuad);
+                else
+                    LeanTween.moveLocal(gameObject, iniTrans, 0.2f).setEase(LeanTweenType.easeOutQuad);
+            }
+
+            alt = !alt;
+
+        }
+    }
+}
