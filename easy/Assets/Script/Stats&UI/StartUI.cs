62f221be399888f3135c578c95474157357ad833
bestdtang <51592725+bestdtang@users.noreply.github.com>
2023-06-30 17:46:56 +0800

    等一个机关介绍板

--- easy/Assets/Script/Stats&UI/StartUI.cs
+++ easy/Assets/Script/Stats&UI/StartUI.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartUI : MonoBehaviour
 {
-    public GameObject enemyBook;
+    public GameObject enemyBook, ProductionGroup;
+    public Button l1, l2, Tutorial, Exit;
+    public bool CheckedTutorial;
+    private void Start()
+    {
+        CheckedTutorial = false;
+        l1.interactable = false;
+        l2.interactable = false;
+        enemyBook.SetActive(false);
+        ProductionGroup.SetActive(false);
+    }
     public void Level1Start()
     {
         SceneManager.LoadScene(1);
@@ -23,11 +34,27 @@ public class StartUI : MonoBehaviour
 
     public void EnemyBookOpen()
     {
+        CheckedTutorial = true;
         enemyBook.SetActive(true);
+        l1.interactable = true;
+        l2.interactable = true;
+        Exit.gameObject.SetActive(false);
     }
-    
+
     public void EnemyBookClose()
     {
         enemyBook.SetActive(false);
+        Exit.gameObject.SetActive(true);
+    }
+    public void GroupOpen()
+    {
+        ProductionGroup.SetActive(true);
+        Exit.gameObject.SetActive(false);
+    }
+
+    public void GroupClose()
+    {
+        ProductionGroup.SetActive(false);
+        Exit.gameObject.SetActive(true);
     }
 }
